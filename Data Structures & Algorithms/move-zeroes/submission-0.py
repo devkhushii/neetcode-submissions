262a43157cce4a2602dc12464b5fadc76a3d9d48
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-07-13 13:37:41 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left=0
+        right=0
+
+        while left!=len(nums):
+            if nums[left]!=0:
+                nums[left],nums[right]=nums[right],nums[left]
+                right+=1
+            left+=1
